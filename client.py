c6c7c1d02abaeb8a45991d9ea367bee4fc310026
Maria <mariahellstrand@yahoo.se>
2020-09-18 10:28:36 +0200

    Created server and client

---
+++ client.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 18 09:35:37 2020
+
+@author: Maria Hellstrand
+"""
+
+import socket
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)      # For UDP
+
+udp_host = socket.gethostname()		# Host IP
+udp_port = 12345			        # specified port to connect
+
+msg = "Hello Python!"
+print("UDP target IP:", udp_host)
+print("UDP target Port:", udp_port)
+
+sock.sendto(msg.encode('utf-8'),(udp_host,udp_port))		# Sending message to UDP server
---
+++ server.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 18 09:32:43 2020
+
+@author: Maria Hellstrand
+"""
+
+import socket
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)      # For UDP
+
+udp_host = socket.gethostname()		        # Host IP
+udp_port = 12345			                # specified port to connect
+
+#print type(sock) ============> 'type' can be used to see type 
+				# of any variable ('sock' here)
+
+sock.bind((udp_host,udp_port))
+
+while True:
+	print("Waiting for client...")
+	data,addr = sock.recvfrom(1024)	        #receive data from client
+	print("Received Messages:",data," from",addr)
